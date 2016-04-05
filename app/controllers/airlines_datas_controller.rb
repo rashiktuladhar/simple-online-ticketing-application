330524245905e4039d78d4856fe1f12b61735b97
Rashik <rashik.tuladhar@outlook.com>
2016-04-06 01:22:02 +0545

    addition of new data initiated with rendered form, params and flash message ! Yet to complete

--- app/controllers/airlines_datas_controller.rb
+++ app/controllers/airlines_datas_controller.rb
@@ -4,9 +4,20 @@ class AirlinesDatasController < ApplicationController
   end
 
   def new
+    @airline = AirlinesDatum.new
   end
 
   def create
+    
+    @airline = AirlinesDatum.new(airline_params)
+
+    if @airline.save
+      flash[:success] = "Airlines added successfully."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
   end
 
   def show
@@ -20,4 +31,12 @@ class AirlinesDatasController < ApplicationController
 
   def destroy
   end
+
+
+  private
+
+  def airline_params
+    params.require(:AirlinesDatum).permit(:airlines_name ,:airlines_address ,:airlines_contact ,:airlines_plane_capacity)
+  end
+
 end
