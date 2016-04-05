ca0684c2d6ad8517cefb58707d1580ab75326b8b
Rashik <rashik.tuladhar@outlook.com>
2016-04-06 00:17:22 +0545

    model created and migrated for airlines data

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160405183144) do
+
+  create_table "airlines_data", force: :cascade do |t|
+    t.string   "airlines_name"
+    t.string   "airlines_address"
+    t.string   "airlines_contact"
+    t.integer  "airlines_plane_capacity"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
 end
